a50e767497819d7ff91f838f0f20cad382842011
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2024-07-29 10:34:37 -0500

    avance

--- src/config/global.js
+++ src/config/global.js
@@ -181,7 +181,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/228138_CF06_DU.zip',
+        download: 'downloads/228138_CF06_DU.pdf',
       },
       {
         icono: 'fas fa-download',
